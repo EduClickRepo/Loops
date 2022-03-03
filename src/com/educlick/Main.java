6d571c2088df4f905e09e6dad2def34af5bc48fa
Camilo Hernandez <camehernandecas@unal.edu.co>
2022-03-02 19:39:37 -0500

    ejercicio loops anidados

--- src/com/educlick/Main.java
+++ src/com/educlick/Main.java
@@ -4,7 +4,19 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+//		for(int contador = 1; contador < 1000; contador = contador + 1) {
+//			System.out.println(contador);
+//			contador = contador - 1;
+//		}
+		
+		for(int i = 1; i < 5; i = i + 1) {
+			for(int x = 1; x < 10; x = x + 1) {
+				System.out.println("el valor de x es: "+ x);
+				System.out.println("el valor de i es: "+ i);
+			}
+			System.out.println("por fuera i es: "+ i);
+		}
+		
 	}
 
 }

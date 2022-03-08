b4418ecc94ca359ec47287998cfe18e51aaba99b
Camilo Hernandez <camehernandecas@unal.edu.co>
2022-03-07 19:04:40 -0500

    explicación break y conitnue

--- src/com/educlick/Main.java
+++ src/com/educlick/Main.java
@@ -9,13 +9,37 @@ public class Main {
 //			contador = contador - 1;
 //		}
 		
-		for(int i = 1; i < 5; i = i + 1) {
-			for(int x = 1; x < 10; x = x + 1) {
-				System.out.println("el valor de x es: "+ x);
-				System.out.println("el valor de i es: "+ i);
+//		for(int i = 1; i < 5; i = i + 1) {
+//			for(int x = 1; x < 10; x = x + 1) {
+//				System.out.println("el valor de x es: "+ x);
+//				System.out.println("el valor de i es: "+ i);
+//			}
+//			System.out.println("por fuera i es: "+ i);
+//		}
+		
+		for(int horas = 0; horas <2; horas++) {
+			if(horas == 1) {
+				break;
 			}
-			System.out.println("por fuera i es: "+ i);
+			for(int minutos = 0; minutos <5; minutos++) {
+				if(minutos == 3) {
+					break;
+				}else {
+					if(minutos == 4) {
+						continue;
+					}
+				}
+				for(int segundos = 0; segundos <4; segundos++) {
+					if(segundos % 2 == 0) {
+						continue;
+					}
+					System.out.println("La hora es: " + horas + ":" + minutos + ":" + segundos);
+				}
+			}
 		}
+		
+		//Ejercicio un temporizador
+		
 		
 	}
 

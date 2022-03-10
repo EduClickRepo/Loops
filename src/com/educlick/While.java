647b565c635ca4edc7b40692544688b8561c0632
Camilo Hernandez <camehernandecas@unal.edu.co>
2022-03-09 19:09:20 -0500

    explicación ciclo while

---
+++ src/com/educlick/While.java
@@ -0,0 +1,82 @@
+package com.educlick;
+
+import java.util.Scanner;
+
+public class While {
+
+	public static void main(String[] args) {
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		int miVariable = 0;
+//		while(true) {
+//			
+//			System.out.println(miVariable);
+//			miVariable++;
+//		}
+		
+//		int miVariable = 0;
+//		while(miVariable < 5) {
+//			System.out.println(miVariable);
+//			miVariable++;
+//		}
+
+		
+//		while(true) {
+//			System.out.println("Si es usuario marque 1");
+//			System.out.println("Si no es usuario marque 2");
+//			
+//			Scanner sc = new Scanner(System.in);
+//	        // entrada de un String
+//	        int seleccionMenu = sc.nextInt();
+//	        
+//	        if(seleccionMenu == 1) {
+//	        	System.out.println("para urgencias marque 1");
+//	        	System.out.println("para citas medicas marque 2");
+//	        	System.out.println("para facturacion marque 3");
+//	        	System.out.println("para salir marque 9");
+//	        	seleccionMenu = sc.nextInt();
+//	        	if(seleccionMenu==9) {
+//	        		break;
+//	        	}
+//	        }else {
+//		        if(seleccionMenu == 2) {
+//		        	System.out.println("para registrarse marque 1");
+//		        	System.out.println("para quejas y reclamos marque 2");
+//		        	System.out.println("para salir marque 9");
+//		        }else {
+//		        	System.out.println("Su opcion no es valida intente nuevamente");
+//		        }
+//	        }
+//		}
+
+//		int miVariable = 0;
+//		while(true) {
+//			System.out.println(miVariable);
+//			miVariable++;
+//			if(miVariable== 5) {
+//				break;
+//			}
+//		}
+		
+//		int x = 21;
+//        do
+//        {
+//            //El código dentro del do se imprime incluso
+//            //si la condición es falsa
+//            System.out.println("Valor de x :" + x);
+//            x++;
+//        }
+//        while (x < 20);
+		
+	}
+
+}
